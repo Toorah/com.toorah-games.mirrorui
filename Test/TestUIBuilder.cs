a62eb19d6f89829a38eb57e3157a9c6750a0b63c
Toorah <tugratoorah@gmail.com>
2021-02-03 02:28:05 +0100

    Update TestUIBuilder.cs
    - added namespace for testing

--- Test/TestUIBuilder.cs
+++ Test/TestUIBuilder.cs
@@ -1,64 +1,68 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
 using Toorah.MirrorUI;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
-public class TestUIBuilder : MonoBehaviour
+namespace ToorahUI.Test
 {
-    Dictionary<Type, BaseUIBuilder> m_uiBuilders = new Dictionary<Type, BaseUIBuilder>();
-    public Transform container;
-    public TestClass test;
-    // Start is called before the first frame update
-    void Start()
-    {
-        test = new TestClass();
-        //m_uiBuilders.Add(typeof(TestClass), new TestClassUIBuilder());
 
-        var uiBuilder = GetBuilder<TestClass>();
-        uiBuilder.BuildUI(test, container);
-    }
 
-    BaseUIBuilder<T> GetBuilder<T>(BindingFlags? flags = null)
+    public class TestUIBuilder : MonoBehaviour
     {
-        if(m_uiBuilders.TryGetValue(typeof(T), out var builder))
+        Dictionary<Type, BaseUIBuilder> m_uiBuilders = new Dictionary<Type, BaseUIBuilder>();
+        public Transform container;
+        public TestClass test;
+        // Start is called before the first frame update
+        void Start()
         {
-            return (BaseUIBuilder<T>)builder;
+            test = new TestClass();
+            //m_uiBuilders.Add(typeof(TestClass), new TestClassUIBuilder());
+
+            var uiBuilder = GetBuilder<TestClass>();
+            uiBuilder.BuildUI(test, container);
         }
-        else
+
+        BaseUIBuilder<T> GetBuilder<T>(BindingFlags? flags = null)
         {
-            Debug.Log($"Created new UI Builder<{typeof(T)}>");
-            if(flags != null)
+            if (m_uiBuilders.TryGetValue(typeof(T), out var builder))
             {
-                var b = new GenericUIBuilder<T>((BindingFlags)flags);
-                m_uiBuilders.Add(typeof(T), b);
-                return b;
+                return (BaseUIBuilder<T>)builder;
             }
             else
             {
-                var b = new GenericUIBuilder<T>();
-                m_uiBuilders.Add(typeof(T), b);
-                return b;
+                Debug.Log($"Created new UI Builder<{typeof(T)}>");
+                if (flags != null)
+                {
+                    var b = new GenericUIBuilder<T>((BindingFlags)flags);
+                    m_uiBuilders.Add(typeof(T), b);
+                    return b;
+                }
+                else
+                {
+                    var b = new GenericUIBuilder<T>();
+                    m_uiBuilders.Add(typeof(T), b);
+                    return b;
+                }
             }
         }
-    }
 
-    [System.Serializable]
-    public class TestClass
-    {
-        [InputHint("Enter Firstname")]
-        public string firstname { get; set; }
-        public string surname { get; set; }
-        [InputHint("Email")]
-        public string address { get; set; }
-        public bool isDeveloper { get; set; }
-        public bool isPatron { get; set; }
-        public bool subscribeToSpam { get; set; }
-        public string[] ArrayTest { get; set; } = new string[] {"A", "B", "C" };
+        [System.Serializable]
+        public class TestClass
+        {
+            [InputHint("Enter Firstname")]
+            public string firstname { get; set; }
+            public string surname { get; set; }
+            [InputHint("Email")]
+            public string address { get; set; }
+            public bool isDeveloper { get; set; }
+            public bool isPatron { get; set; }
+            public bool subscribeToSpam { get; set; }
+            public string[] ArrayTest { get; set; } = new string[] { "A", "B", "C" };
 
-        private string sessionId { get; set; }
+            private string sessionId { get; set; }
+        }
+
     }
 
 }
